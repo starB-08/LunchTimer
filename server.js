def6ddcea1b3e72d756bf2424eb71271543af81d
StarB_ <uve0508@naver.com>
2025-10-19 07:47:24 +0900

    v 0.1

--- server.js
+++ server.js
@@ -1,35 +1,94 @@
 const express = require("express");
-const app = express();
-const port = process.env.PORT || 80;
-
 const cors = require("cors");
+const fs = require("fs");
+const path = require("path");
+const { Server } = require("socket.io");
+const http = require("http");
+
+const app = express();
 app.use(cors());
+const server = http.createServer(app);
+const io = new Server(server);
+
+const port = process.env.PORT || 80;
+const dataPath = path.join(__dirname, "data", "data.json");
+
+io.on("connection", (socket) => {
+  console.log("viewer connected:", socket.id);
+  socket.emit("nowClass", nowClass);
+});
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.send("hello world!");
 });
 
 app.get("/viewer", (req, res) => {
-  res.sendFile(__dirname + "/frontend/viewer/index.html");
+  res.sendFile(__dirname + "/frontend/viewer.html");
 });
 
 app.get("/control", (req, res) => {
-  res.sendFile(__dirname + "/frontend/index.html");
+  res.sendFile(__dirname + "/frontend/control.html");
 });
 
-app.get("/sound/:name", (req, res) => {
-  const { name } = req.params;
-  if (name == "dog") {
-    res.json({ sound: "멍멍" });
-  } else if (name == "cat") {
-    res.json({ sound: "야옹" });
-  } else if (name == "pig") {
-    res.json({ sound: "꿀꿀" });
-  } else {
-    res.json({ sound: name });
-  }
+app.get("/admin", (req, res) => {
+  res.sendFile(__dirname + "/frontend/admin.html");
 });
 
-app.listen(port, () => {
+app.get("/nextClass/:i", (req, res) => {
+  const i = Number(req.params.i);
+  nowClass[i] += 1;
+  if (nowClass[i] > maxClass[i]) nowClass[i] = 1;
+  io.emit("nowClass", nowClass);
+  io.emit("flicker", i);
+  res.send(nowClass);
+});
+
+app.get("/getClass", (req, res) => {
+  res.send(nowClass);
+});
+
+app.get("/read", (req, res) => {
+  fs.readFile(dataPath, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    try {
+      _data = JSON.parse(data);
+      nowClass = _data.firstClass;
+      maxClass = _data.classAmount;
+      res.json(_data);
+    } catch (parseErr) {
+      console.error("JSON 파싱 오류:", parseErr);
+      res.status(500).json({ error: "JSON 형식이 잘못되었습니다." });
+    }
+  });
+});
+
+app.get("/write/:v", (req, res) => {
+  const value = req.params.v;
+  console.log(value);
+  fs.writeFile(dataPath, value, (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+  });
+});
+
+app.get("/getMaxClass", (req, res) => {
+  res.send(maxClass);
+});
+
+server.listen(port, () => {
   console.log(`server is listening at localhost:${port}`);
+  fetch("http://localhost/read");
 });
+
+//----------------------------------------------------------//
+
+let _data = JSON.parse('{ "classAmount": [10,10,13], "firstClass": [1,1,1] }');
+let nowClass = [1, 1, 1];
+let maxClass = [10, 10, 13];
